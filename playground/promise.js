ed80ca7bbbf0244b2390f8575b804d4c73a63f51
Brandon Jolie <brandonjolie@gmail.com>
2021-11-10 12:15:37 -0500

    added async

--- playground/promise.js
+++ playground/promise.js
@@ -3,14 +3,6 @@ const User = require('../src/models/user')
 
 id = '618b5a525864632e4c3db376'
 
-// User.findByIdAndUpdate('618b55e28aff7c34dcbda311', { age: 24 }).then((user) => {
-//     console.log(user)
-//     return User.countDocuments({ age: 24 })
-// }).then((result) => {
-//     console.log(result)
-// }).catch((e) => {
-//     console.log(e)
-// })
 
 const updateAgeandCount = async (id, age) => {
     const user = await User.findByIdAndUpdate(id, { age })
