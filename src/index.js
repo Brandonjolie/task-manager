452818929df52347f8ada172a4c1b24154ed90e9
Brandon Jolie <brandonjolie@gmail.com>
2021-11-12 11:51:07 -0500

    removing comments

--- src/index.js
+++ src/index.js
@@ -13,19 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log(`Server running on ${port}`)
 })
-
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById("618d4caf2c7eaaf0e8aab118")
-    // await task.populate('owner')
-    // console.log(task.owner)
-
-    // const user = await User.findById('618d4bb8dff0d896fa769093')
-    // await user.populate('tasks')
-    // console.log(user.tasks)
-}
-
-main()
